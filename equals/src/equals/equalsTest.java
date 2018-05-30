4730a8078204f1ee8c6749ed690c4591f94e6104
Rodrigo_P <r.pachecom@alumnos.urjc.es>
2018-05-30 11:04:27 +0200

    Primer test Node Coverage

--- equals/src/equals/equalsTest.java
+++ equals/src/equals/equalsTest.java
@@ -2,13 +2,28 @@ package equals;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.awt.List;
+
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
 class equalsTest {
+	List<String> list1;
+	List<String> list2;
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
+	@Before
+	public void setUp() throws Exception {
+		list1 = new ArrayList<String>();
+		list2 = new ArrayList<String>();
 	}
+	
+	// Apartado 3 - Node Coverage
+	
+	@Test
+	void test12() {
+		assertTrue(list1.equals(list1));
+	}
+	
+	
 
 }
