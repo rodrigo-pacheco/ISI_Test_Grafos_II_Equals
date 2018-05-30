97a1b85c5ee0025c4b75ee22318354472b881df3
Rodrigo_P <r.pachecom@alumnos.urjc.es>
2018-05-30 11:00:20 +0200

    Creo fichero de Test

---
+++ equals/src/equals/equalsTest.java
@@ -0,0 +1,14 @@
+package equals;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class equalsTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
