b4653c89b9b964da63b218ddbb66c76096ec0c80
Rodrigo_P <r.pachecom@alumnos.urjc.es>
2018-05-30 11:21:31 +0200

    Test Node Coverage completados

--- equals/src/equals/equalsTest.java
+++ equals/src/equals/equalsTest.java
@@ -20,16 +20,19 @@ public class equalsTest {
 	
 	//	Node Coverage
 	
+	// Nodos: 1,2	Edges: 1-2
 	@Test
 	public void test12() {
 		assertTrue(list1.equals(list1));
 	}
 	
+	// Nodos: 1,3,4	Edges: 1-3, 3-4
 	@Test
 	public void test134() {
 		assertFalse(list1.equals(new Integer(0)));
 	}
 	
+	// Nodos: 1,3,5,6,7,8	Edges: 1-3, 3-5, 5-6, 6-7, 7-8
 	@Test
 	public void test135678() {
 		list1.add("a");
@@ -37,10 +40,13 @@ public class equalsTest {
 		assertFalse(list1.equals(list2));
 	}
 	
+	// Nodos: 1,3,5,6,7,9,6,10	Edges: 1-3, 3-5, 5-6, 6-7, 7-9, 9-6, 6-10
 	@Test
-	public void test13561910() {
+	public void test13567910() {
 		list1.add("Hola mundo");
 		list2.add("Hola mundo");
 		assertTrue(list1.equals(list2));
 	}
+	
+
 }
